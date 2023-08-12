7a51b457f9c607e026e750c597a63baafee90436
0qsenxx <semen.antipyuk@gmail.com>
2023-08-12 15:37:40 +0300

    hw-js-1

---
+++ js/hw-js-1.js
@@ -0,0 +1,12 @@
+// #1
+const age = 10;
+console.log(age);
+
+// #2
+const message = "JavaScript is fun!";
+const test = message.length;
+console.log(test);
+
+// #3
+const comment = 30;
+console.log(comment);
