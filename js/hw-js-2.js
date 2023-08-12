ac8072be30950f2234bae5ac19311ea35eab8aea
0qsenxx <semen.antipyuk@gmail.com>
2023-08-12 15:38:35 +0300

    hw-js-2

---
+++ js/hw-js-2.js
@@ -0,0 +1,49 @@
+// #1
+const str = "Привіт";
+const strMode = typeof str;
+const num = 123;
+const numMode = typeof num;
+const flag = true;
+const flagMode = typeof flag;
+const txt = "true";
+const txtMode = typeof txt;
+console.log(strMode);
+console.log(numMode);
+console.log(flagMode);
+console.log(txtMode);
+
+// #2
+const firstName = "Semen";
+const lastName = "Antipyuk";
+const price = 5000;
+const message = "Хочете отримати знажку?";
+let isOpen = true;
+let isClose = false;
+const age = 13;
+let test = "Оберіть правильну відповідь";
+let text = "javaScript is fun";
+let name = "iPhone 14pro max";
+
+// #3
+console.log("before click");
+let userProfile = prompt("введіть імʼя користувача");
+console.log(userProfile);
+console.log("after click");
+
+// #4
+const myAge = 13;
+const friendAge = 13.5;
+const generalAge = myAge + friendAge;
+alert(generalAge);
+
+// #5
+const message2 = "Я навчаюся в GoITeens в групі:";
+const group = "Front-end_1y_2 менші";
+console.log(message2, group);
+
+// #6
+const computerName = "MacBook Pro 13″,";
+let price2 = 10000;
+console.log("обрано", computerName, "ціна за штуку", price2, "кредитів");
+price2 = 20000;
+console.log("обрано", computerName, "ціна за штуку", price2, "кредитів");
