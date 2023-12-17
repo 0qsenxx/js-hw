71cc0f83c1f3de6191c1ba31661592921837918c
0qsenxx <semen.antipyuk@gmail.com>
2023-12-17 20:00:29 +0200

    hw-js-31

---
+++ js/hw-js-31.js
@@ -0,0 +1,32 @@
+// ?1
+// const imgs = document.querySelectorAll('img');
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 1,
+};
+const loadersRef = document.querySelectorAll(".loader");
+
+const callback = (entries, observer) => {
+  entries.forEach((entrie) => {
+    if (entrie.isIntersecting && entrie.target.hasAttribute("data-src")) {
+      entrie.target.src = entrie.target.getAttribute("data-src");
+      entrie.target.classList.add("visible");
+      loadersRef.forEach((loader) => {
+        loader.style.display = "none";
+      });
+    } else if (!entrie.isIntersecting) {
+      entrie.target.classList.remove("visible");
+      loadersRef.forEach((loader) => {
+        loader.style.display = "block";
+      });
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+document.querySelectorAll(".imgs__item img").forEach((img) => {
+  observer.observe(img);
+});
